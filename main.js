adf2ee2a8dc095e1b21c2d9f81d408f5f168f131
Eugenia Necul <francisca.neculm@gmail.com>
2026-03-10 00:08:33 -0300

    Se trabaja en main.js, creando funciones

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
-console.log("Hola")
+alert("¡Bienvenido a Tu Calculadora Personal! \n\n\n\nOperaciones que puedes realizar +,-,*,/  \nSe recomienda abrir la consola con F12")
 
-alert("tortuga")
+function calculadora(){
 
-let prueba = prompt("escriba aquí")
+    const primerNumero = prompt("Ingrese Número Entero o Decimal")
+    console.log("Primer Número Seleccionado:", primerNumero)
+    
+
+    const operacion = prompt("ingrese operación")
+    console.log("Operación Seleccionado:", operacion)
+
+
+    const segundoNumero = prompt("Ingrese Número Entero o Decimal")
+    console.log("Segundo Número Seleccionado:", segundoNumero)
+   
+
+    switch(operacion){
+
+        case("+"):
+
+        let resultadoSuma =  parseFloat(primerNumero) + parseFloat(segundoNumero)
+        console.log(resultadoSuma)
+
+        break;
+
+        case("-"):
+
+        let resultadoResta =  parseFloat(primerNumero) - parseFloat(segundoNumero)
+        console.log(resultadoResta)
+
+        break;
+
+        case("*"):
+
+        let resultadoMulti =  parseFloat(primerNumero) * parseFloat(segundoNumero)
+        console.log(resultadoMulti)
+
+        break;
+
+        case("/"):
+
+        let resultadoDivision =  parseFloat(primerNumero) / parseFloat(segundoNumero)
+        console.log(resultadoDivision)
+
+        break;
+
+        default:
+            alert("Operación Invalida")
+
+    }
+}
+
+calculadora()
+
+
