fd54fcfb5cf6e8686fe759057e0dbb10f5e97642
Eugenia Necul <francisca.neculm@gmail.com>
2026-03-10 23:37:48 -0300

    se implementan los sistemas de validación de datos, se crean alert, se crea readme y se finaliza el proyecto

---
+++ README.md
@@ -0,0 +1,24 @@
+# CALCULADORA PERSONAL
+
+*Aplicación de Consola para proyecto de Bootcamp Front-End*
+
+**Link de Repositorio** https://github.com/Eugenia-Necul/APLICACI-N-DE-CONSOLA
+
+## Caracteristicas Actuales
+
+- Puede ingresar números enteros o decimales
+- Operaciones matemáticas básicas
+- Se puede confirmar si necesita otra operación
+
+
+## Caracteristicas por implementar
+
+- Mejoras en Accesibilidad
+- Mejoramiento estético de la interfaz
+
+
+## Herramientas utilizadas 
+
+- HTML5
+- Main.js
+- Git Hub
--- main.js
+++ main.js
@@ -1,54 +1,94 @@
 alert("¡Bienvenido a Tu Calculadora Personal! \n\n\n\nOperaciones que puedes realizar +,-,*,/  \nSe recomienda abrir la consola con F12")
 
+const signosValidos = ["+", "-", "*", "/"]
+
+
 function calculadora(){
 
-    const primerNumero = prompt("Ingrese Número Entero o Decimal")
+    const primerNumero = parseFloat(prompt("Ingrese Primer Número (Número Entero o Decimal)"))
     console.log("Primer Número Seleccionado:", primerNumero)
-    
 
+    if (isNaN(primerNumero)){
+
+        alert("Ingrese Números")
+        calculadora()
+        return
+    
+    }
+    
     const operacion = prompt("ingrese operación")
     console.log("Operación Seleccionado:", operacion)
 
+    if (!signosValidos.includes(operacion)){
+        
+        alert("ingrese signo +,-,*,/")
+        calculadora()
+        return
+        }
+        
 
-    const segundoNumero = prompt("Ingrese Número Entero o Decimal")
+
+    const segundoNumero = parseFloat(prompt("Ingrese Segundo Número (Número Entero o Decimal)"))
     console.log("Segundo Número Seleccionado:", segundoNumero)
-   
 
+    if (isNaN(segundoNumero)) {
+
+        alert("Ingrese Números")
+        calculadora()
+        return
+        }
+    
+    else{
+   
     switch(operacion){
 
         case("+"):
 
-        let resultadoSuma =  parseFloat(primerNumero) + parseFloat(segundoNumero)
+        let resultadoSuma =  primerNumero + segundoNumero
         console.log(resultadoSuma)
+        alert(`la suma de ${primerNumero} + ${segundoNumero} es: ${resultadoSuma}`)
 
         break;
 
         case("-"):
 
-        let resultadoResta =  parseFloat(primerNumero) - parseFloat(segundoNumero)
+        let resultadoResta =  primerNumero - segundoNumero
         console.log(resultadoResta)
+        alert(`la resta de ${primerNumero} - ${segundoNumero} es: ${resultadoResta}`)
 
         break;
 
         case("*"):
 
-        let resultadoMulti =  parseFloat(primerNumero) * parseFloat(segundoNumero)
+        let resultadoMulti =  primerNumero * segundoNumero
         console.log(resultadoMulti)
+        alert(`la multiplicación de ${primerNumero} * ${segundoNumero} es: ${resultadoMulti}`)
 
         break;
 
         case("/"):
 
-        let resultadoDivision =  parseFloat(primerNumero) / parseFloat(segundoNumero)
+        if (segundoNumero == 0) {
+            alert("No se puede dividir por 0")
+        }
+
+        else {
+        let resultadoDivision =  primerNumero / segundoNumero
         console.log(resultadoDivision)
+        alert(`la división de ${primerNumero} / ${segundoNumero} es: ${resultadoDivision}`)
+        }
 
         break;
 
         default:
             alert("Operación Invalida")
 
-    }
-}
+        }
+        const otraOperacion = confirm("¿Deseas realizar otra operación?")
+            if (otraOperacion) {
+            calculadora()
+        }
+        }}
 
 calculadora()
 
